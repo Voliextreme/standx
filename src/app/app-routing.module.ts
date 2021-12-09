0448e78e40c4daad8a65830290c498b86694b3d2
Voliextreme <voliextreme@gmail.com>
2021-12-09 17:47:30 +0000

    yha
    Co-authored-by: José Manuel Rocha de Castro <a20351@alunos.ipca.pt>
    Co-authored-by: Lucas Silva <LucasSilva1503@users.noreply.github.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
-import { ChangepwComponent } from './pages/changepw/changepw.component';
-import { CreateuserComponent } from './pages/createuser/createuser.component';
+import { ChangepwComponent } from './pages/change-pw/changepw.component';
+import { CreateuserComponent } from './pages/create-user/createuser.component';
+import { EditProfileComponent } from './pages/edit-profile/edit-profile.component';
+import { FourOfourComponent } from './pages/four-ofour/four-ofour.component';
 
 const routes: Routes = [
   {path: "", component: LoginComponent},
   { path: 'login', component: LoginComponent },
   { path: 'changepw', component: ChangepwComponent },
+  { path: 'editProfile', component: EditProfileComponent },
   { path: 'createUser', component: CreateuserComponent },
   { path: 'home', component: HomeComponent},
+  { path: '**', component: FourOfourComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
-import { ChangepwComponent } from './pages/changepw/changepw.component';
-import { CreateuserComponent } from './pages/createuser/createuser.component';
+import { EditProfileComponent } from './pages/edit-profile/edit-profile.component';
+import { FourOfourComponent } from './pages/four-ofour/four-ofour.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ChangepwComponent } from './pages/change-pw/changepw.component';
+import { CreateuserComponent } from './pages/create-user/createuser.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,10 @@ import { CreateuserComponent } from './pages/createuser/createuser.component';
     LoginComponent,
     HomeComponent,
     ChangepwComponent,
-    CreateuserComponent
+    CreateuserComponent,
+    EditProfileComponent,
+    NavBarComponent,
+    FourOfourComponent
   ],
   imports: [
     BrowserModule,

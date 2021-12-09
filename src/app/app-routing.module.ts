d4c0feb6b4454d1c6f71f6dc5981c252f425fbba
Voliextreme <voliextreme@gmail.com>
2021-12-09 02:05:24 +0000

    Start project with few components hmtl and css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ChangepwComponent } from './pages/changepw/changepw.component';
+import { CreateuserComponent } from './pages/createuser/createuser.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: LoginComponent},
+  { path: 'login', component: LoginComponent },
+  { path: 'changepw', component: ChangepwComponent },
+  { path: 'createUser', component: CreateuserComponent },
+  { path: 'home', component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ChangepwComponent } from './pages/changepw/changepw.component';
+import { CreateuserComponent } from './pages/createuser/createuser.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    ChangepwComponent,
+    CreateuserComponent
   ],
   imports: [
     BrowserModule,
